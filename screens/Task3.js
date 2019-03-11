bedea0adfa95c8bb177a0ebbc8e6fea2a64c3092
osdnk <micosa97@gmail.com>
2019-03-11 14:21:57 +0100

    Add task 3

---
+++ screens/Task3.js
@@ -0,0 +1,201 @@
+import React from 'react';
+import {
+  Animated,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { GestureHandler } from 'expo';
+
+const { PanGestureHandler, State, PinchGestureHandler } = GestureHandler;
+
+
+export default class Task1 extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  translateX = new Animated.Value(0);
+  translateY = new Animated.Value(0);
+  prevX = 0;
+  prevY = 0;
+
+  onGestureEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationX: this.translateX,
+          translationY: this.translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true }
+  );
+
+  onHandlerStateChange = ({ nativeEvent: { oldState, translationX, translationY } }) => {
+    if (oldState === State.ACTIVE) {
+      this.prevX += translationX;
+      this.prevY += translationY;
+      this.translateX.setValue(0);
+      this.translateY.setValue(0);
+      this.translateX.setOffset(this.prevX);
+      this.translateY.setOffset(this.prevY);
+    }
+  };
+
+  baseScale = new Animated.Value(1);
+  pinchScale = new Animated.Value(1);
+  scale = Animated.multiply(this.baseScale, this.pinchScale);
+  lastScale = 1;
+  onPinchGestureEvent = Animated.event(
+    [{ nativeEvent: { scale: this.pinchScale } }],
+    { useNativeDriver: true }
+  );
+
+  onPinchHandlerStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      this.lastScale *= event.nativeEvent.scale;
+      this.baseScale.setValue(this.lastScale);
+      this.pinchScale.setValue(1);
+    }
+  };
+
+  pinch = React.createRef();
+  pan = React.createRef();
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PinchGestureHandler
+          ref={this.pinch}
+          simultaneousHandlers={this.ref}
+          onGestureEvent={this.onPinchGestureEvent}
+          onHandlerStateChange={this.onPinchHandlerStateChange}>
+          <Animated.View>
+            <PanGestureHandler
+              ref={this.pan}
+              simultaneousHandlers={this.pinch}
+              onGestureEvent={this.onGestureEvent}
+              onHandlerStateChange={this.onHandlerStateChange}
+            >
+              <Animated.View
+                style={[
+                  styles.box,
+                  {
+                    transform: [
+                      /*Does order matter?*/
+                      { scale: this.scale },
+                      { translateX: this.translateX },
+                      { translateY: this.translateY },
+                    ],
+                  }
+                ]}
+              >
+                <Text>
+                  Rewrite using reanimated
+                </Text>
+              </Animated.View>
+            </PanGestureHandler>
+          </Animated.View>
+        </PinchGestureHandler>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  box: {
+    width: 100,
+    backgroundColor: 'yellow',
+    height: 100,
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
