eee3593e50e919b99ce7bf5affbc12c09e453ab0
osdnk <micosa97@gmail.com>
2019-03-14 16:21:42 +0100

    Add solution 1.

--- screens/Task1.js
+++ screens/Task1.js
@@ -5,6 +5,36 @@ import { CliectSays } from './ClientSays'
 
 const  { PanGestureHandler, State } = GestureHandler
 export default class Task1 extends React.Component {
+  translateX = new Animated.Value(0)
+  translateY = new Animated.Value(0)
+  prevX = 0;
+  prevY = 0;
+
+  onGestureEvent=Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationX: this.translateX,
+          translationY: this.translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true }
+  );
+
+  onHandlerStateChange = ({ nativeEvent:
+    { oldState, translationX, translationY }
+                          }) => {
+    if (oldState === State.ACTIVE) {
+      this.prevX += translationX
+      this.prevY += translationY
+      this.translateX.setValue(0)
+      this.translateY.setValue(0)
+      this.translateX.setOffset(this.prevX)
+      this.translateY.setOffset(this.prevY)
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -13,7 +43,22 @@ export default class Task1 extends React.Component {
           It will be some super component some day but now I want to make
           it draggable. Could you help me?"
         />
-        <View style={styles.box}/>
+        <PanGestureHandler
+          onGestureEvent={this.onGestureEvent}
+          onHandlerStateChange={this.onHandlerStateChange}
+        >
+          <Animated.View
+            style={[
+              styles.box,
+              {
+                transform: [
+                  { translateX: this.translateX },
+                  { translateY: this.translateY },
+                ],
+              }
+            ]}
+          />
+        </PanGestureHandler>
       </View>
     )
   }
