aa666f2b6241a731f228b898f33bf7d518acae70
osdnk <micosa97@gmail.com>
2019-03-28 15:59:14 +0100

    Add task 0

--- screens/Task0.js
+++ screens/Task0.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Animated, StyleSheet, View, ScrollView } from 'react-native'
 import { GestureHandler } from 'expo'
 import { CliectSays } from './ClientSays'
+import { MonoText } from '../components/StyledText'
 
 const  { PanGestureHandler, State } = GestureHandler
 export default class Task0 extends React.Component {
@@ -9,23 +10,26 @@ export default class Task0 extends React.Component {
     return (
       <View style={styles.container}>
         <ScrollView>
-          At vero eos et accusamus et iusto odio dignissimos
-          ducimus qui blanditiis praesentium voluptatum deleniti
-          atque corrupti quos dolores et quas molestias excepturi
-          sint occaecati cupiditate non provident, similique
-          sunt in culpa qui officia deserunt mollitia animi,
-          id est laborum et dolorum fuga. Et harum quidem
-          rerum facilis est et expedita distinctio. Nam libero
-          tempore, cum soluta nobis est eligendi optio cumque
-          nihil impedit quo minus id quod maxime placeat facere
-          possimus, omnis voluptas assumenda est, omnis dolor
-          repellendus. Temporibus autem quibusdam et aut officiis
-          debitis aut rerum necessitatibus saepe eveniet ut et
-          voluptates repudiandae sint et molestiae non recusandae.
-          Itaque earum rerum hic tenetur a sapiente delectus, ut aut
-          reiciendis voluptatibus maiores alias consequatur aut
-          perferendis doloribus asperiores repellat.
+          <MonoText>
+            At vero eos et accusamus et iusto odio dignissimos
+            ducimus qui blanditiis praesentium voluptatum deleniti
+            atque corrupti quos dolores et quas molestias excepturi
+            sint occaecati cupiditate non provident, similique
+            sunt in culpa qui officia deserunt mollitia animi,
+            id est laborum et dolorum fuga. Et harum quidem
+            rerum facilis est et expedita distinctio. Nam libero
+            tempore, cum soluta nobis est eligendi optio cumque
+            nihil impedit quo minus id quod maxime placeat facere
+            possimus, omnis voluptas assumenda est, omnis dolor
+            repellendus. Temporibus autem quibusdam et aut officiis
+            debitis aut rerum necessitatibus saepe eveniet ut et
+            voluptates repudiandae sint et molestiae non recusandae.
+            Itaque earum rerum hic tenetur a sapiente delectus, ut aut
+            reiciendis voluptatibus maiores alias consequatur aut
+            perferendis doloribus asperiores repellat.
+          </MonoText>
         </ScrollView>
+        <View style={styles.box}/>
       </View>
     )
   }
