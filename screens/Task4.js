8a6424044980da5797a5ce2d9922965220dedd92
osdnk <micosa97@gmail.com>
2019-04-02 02:46:27 +0200

    enhance 4

--- screens/Task4.js
+++ screens/Task4.js
@@ -10,7 +10,36 @@ const {
   State,
 } = GestureHandler
 
-const { set, cond, block, eq, add, Value, sub,event, diff, multiply, clockRunning, startClock, stopClock, decay, Clock } = Animated
+const { set, Easing, timing, cond, block, eq, add, Value, sub,event, diff, multiply, clockRunning, startClock, stopClock, decay, Clock } = Animated
+
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    frameTime: new Value(0),
+    time: new Value(0),
+  };
+
+  const config = {
+    toValue: new Value(0),
+    duration: 300,
+    easing: Easing.inOut(Easing.cubic),
+  };
+
+  return [
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.frameTime, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(config.toValue, dest),
+      startClock(clock),
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, stopClock(clock)),
+    state.position,
+  ];
+}
 
 function withPreservingOffset(drag, state) {
   const prev = new Animated.Value(0)
